804069ee9ed81b75c03d087fc9f26a5d26915cea
Ronald Niz <ronaldalexisniznunez@gmail.com>
2025-10-17 17:17:58 -0300

    Feat: Agrega Vista Roles.Create

--- app/Models/Admin/Permiso.php
+++ app/Models/Admin/Permiso.php
@@ -2,10 +2,27 @@
 
 namespace App\Models\Admin;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 use Spatie\Permission\Models\Permission;
 
-class Permiso extends Permission
+class Permiso extends Permission implements Auditable
 {
-    //
+    use \OwenIt\Auditing\Auditable;
+
+    /*
+    |---------------------------------------
+    | RELACIONES DE AUDITORIA DE LA TABLA
+    |---------------------------------------
+    */
+    public function creadoPor()
+    {
+        return $this->belongsTo(User::class, 'creadoPor');
+    }
+
+    public function actualizadoPor()
+    {
+        return $this->belongsTo(User::class, 'actualizadoPor');
+    }
 }
