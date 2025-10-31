f51a70d354c5519269bb2f418459c1e2a8fbbb66
Ronald Niz <ronaldalexisniznunez@gmail.com>
2025-10-31 14:29:33 -0300

    Feat: Agrega Migraciones Generales

---
+++ database/migrations/2025_10_30_141723_create_SUCURSALES_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('SUCURSALES', function (Blueprint $table) {
+            $table->id();
+            $table->string('sucursal', 50);
+            $table->string('razon_social', 75);
+            $table->string('ruc', 20);
+            $table->string('correo', 50);
+            $table->string('direccion', 100);
+            $table->string('telefono', 20);
+            $table->foreignId('empresa_id')->constrained('EMPRESAS')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('SUCURSALES');
+    }
+};
