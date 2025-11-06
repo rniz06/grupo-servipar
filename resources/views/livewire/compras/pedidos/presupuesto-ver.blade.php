8c48e340dda93ea7ade6a03cbdfbc25ff7f71185
Ronald Niz <ronaldalexisniznunez@gmail.com>
2025-11-06 09:59:01 -0300

    Feat: Agregar al modal de presupuesto ver detalle El precio total por ítem y total general compras.pedidos.show

--- resources/views/livewire/compras/pedidos/presupuesto-ver.blade.php
+++ resources/views/livewire/compras/pedidos/presupuesto-ver.blade.php
@@ -10,18 +10,34 @@
                         <tr>
                             <th>Item</th>
                             <th>Cantidad</th>
-                            <th>Precio</th>
+                            <th>Precio Unitario</th>
+                            <th>Total</th>
                         </tr>
                     </thead>
                     <tbody>
+                        @php
+                            $total_general = 0;
+                        @endphp
+
                         @foreach ($detalles as $detalle)
+                            @php
+                                $total_item = $detalle->precio * $detalle->cantidad;
+                                $total_general += $total_item;
+                            @endphp
                             <tr>
                                 <td>{{ $detalle->producto->nombre ?? 'S/D' }}</td>
                                 <td>{{ $detalle->cantidad ?? 'S/D' }}</td>
-                                <td>{{ $detalle->precio ?? 'S/D' }}</td>
+                                <td>{{ number_format($detalle->precio, 0, ',', '.') ?? 'S/D' }}</td>
+                                <td>{{ number_format($total_item, 0, ',', '.') ?? 'S/D' }}</td>
                             </tr>
                         @endforeach
                     </tbody>
+                    <tfoot>
+                        <tr class="fw-bold">
+                            <td colspan="3" class="text-end">TOTAL GENERAL:</td>
+                            <td>{{ number_format($total_general, 0, ',', '.') }}</td>
+                        </tr>
+                    </tfoot>
                 </table>
             </div>
         </div>
