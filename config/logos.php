54d2936aa687eedadf89c8ca4dd679fcc6f4913a
Ronald Niz <ronaldalexisniznunez@gmail.com>
2025-11-15 09:41:27 -0300

    Fix: Correcciones de vista

--- config/logos.php
+++ config/logos.php
@@ -8,7 +8,9 @@ return [
     |-------------------------------------
     */
     // 'por-defecto' => public_path('vendor/adminlte/dist/img/AdminLTELogo.png'),
-    'principal' => public_path('img/logos/grupo-servipar.webp'),
+    'principal' => public_path('img/logos/rubilock.webp'),
+
+    // 'principal' => public_path('img/logos/grupo-servipar.webp'),
 
     'accion-360' => public_path('img/logos/accion-360.webp'),
 
